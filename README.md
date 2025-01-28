d64cdca85978c8f15fd5334ebe6931bd15fe81bb
DEGETEL - RUYSSCHAERT, Franck <fruyssch@bouyguestelecom.fr>
2025-01-28 22:51:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# ⚛️ Gql Files Def
+
+This npm Codegen plugin automatically generates TypeScript definition files (.d.ts) for GraphQL files (.gql), making it easier to integrate and ensure strict typing in TypeScript applications.
+
+---
+
+## Usage
+
+- **Installation**
+```shell
+npm i -D gql-files-def
+```
+
+- **Configuration**: Add the plugin to your codegen config
+```typescript
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  documents: ['src/**/*.gql', 'src/**/*.graphql'],
+  schema: './schema.graphql',
+  generates: {
+    './src/__graphql__/graphql-operations.ts': {
+      plugins: ['typescript', 'typescript-operations', './tools/gql-type-def.js']
+    }
+  }
+};
+
+export default config;
+```
+
+---
+
+## Advice
+
+###
+
+### With [⚡ Vite](https://github.com/vitejs/vite)
+
+Use the plugin [vite-plugin-graphql-loader](https://www.npmjs.com/package/vite-plugin-graphql-loader) for importing GraphQL files
+
+### Typing schema
+
+Use the plugin [Schema-ast](https://the-guild.dev/graphql/codegen/plugins/other/schema-ast) so that your tools (IDE) can offer you typing
+
+```json
+{
+  //...
+  generates: {
+    './schema.graphql': {
+      plugins: [
+        'schema-ast'
+      ],
+      config: {
+        includeDirectives: true
+      }
+    }
+  }
+  //...
+}
+```
+
+---
+
+[MIT](./LICENSE) License © [Franck RST](https://github.com/Franckrst)
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import type { PluginFunction } from '@graphql-codegen/plugin-helpers'
+import type { OperationDefinitionNode } from 'graphql/language/ast'
+import { writeFileSync } from 'node:fs'
+import { relative } from 'node:path'
+import * as process from 'node:process'
+
+interface DocRsult { location: string, path: string, def: { key: string, name: string }[] }
+
+export const plugin: PluginFunction = (schema, documents, config, info) => {
+  if (!info?.outputFile) {
+    throw new Error('Need output file')
+  }
+  const docResult = documents.reduce((acc, doc) => {
+    return [
+      ...acc,
+      {
+        location: doc.location || '',
+        path: relative(process.cwd(), doc.location || ''),
+        def: (doc.document?.definitions as OperationDefinitionNode[])
+          .filter(d => !!d?.name?.value)
+          .map(d => d?.name?.value || '')
+          .reduce((subAcc, name) => {
+            return [
+              ...subAcc,
+              {
+                key: name,
+                name: String(name).charAt(0).toUpperCase() + String(name).slice(1),
+              },
+            ]
+          }, [] as { key: string, name: string }[]) || [],
+      },
+    ]
+  }, [] as DocRsult[])
+
+  for (const doc of docResult) {
+    const inportPath = relative(doc.location.split('/').slice(0, -1).join('/'), info.outputFile)
+    writeFileSync(`${doc.location}.d.ts`, `
+import { TypedDocumentNode } from '@graphql-typed-document-node/core';
+import type {${doc.def.map(({ name }) => `${name}Query, ${name}QueryVariables`).join(', ')}} from "${inportPath}";
+${doc.def.map(({ name, key }) => `export const ${key}: TypedDocumentNode<${name}Query, ${name}QueryVariables>;`).join('\n')}
+`)
+  }
+
+  return `// All type declarations are next to your query files `
+}
